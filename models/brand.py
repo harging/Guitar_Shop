dd011a5c1b5f695db1e6fec5581ec0bd6a00c367
Gavin Hargin <gavin.hargin@gmail.com>
2021-11-12 14:51:14 +0000

    started project

---
+++ models/brand.py
@@ -0,0 +1,9 @@
+class Brand:
+
+    def __init__(self, brand_id, name, active = True):
+        self.brand_it = brand_id
+        self.name = name
+        self.active = active
+
+    def set_active_status(Brand):
+        pass
---
+++ models/item.py
@@ -0,0 +1,9 @@
+class Item:
+
+    def __init__(self, item_id, name, description, stock_quantity, buying_cost, selling_price):
+        self.item_id = item_id
+        self.name = name
+        self.description = description
+        self.stock_quantity = stock_quantity
+        self.buying_cost = buying_cost
+        self.selling_price = selling_price
